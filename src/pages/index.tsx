ab819c56393f8cfec8d5a375991e632581e2e895
Henrique Campaner <henrique.campaner@live.com>
2021-03-30 12:22:48 +0100

    add document next

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,18 @@
 import style from '../styles/home.module.scss'
+import Head from 'next/head'
 
 export default function Home() {
   return (
+    <>
+    <Head>
+        <title>
+          Home | ig.news
+        </title>
+      </Head>
     <h1 className={style.title}>
+      
       Hello <span>World</span>
     </h1>
+    </>
   )
 }
