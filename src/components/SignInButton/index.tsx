445bdc0cccf8f7d76c0ed0dcb08ba401432cbdc5
Henrique Campaner <henrique.campaner@live.com>
2021-03-31 17:35:30 +0100

    add github auth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,18 +1,27 @@
 import {FaGithub} from 'react-icons/fa'
 import {FiX} from 'react-icons/fi'
 import styles from './signInButton.module.scss'
+import {signIn, signout, useSession} from 'next-auth/client'
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const [session] = useSession()
 
-  return isUserLoggedIn ? (
-    <button type="button" className={styles.signInButton}>
+  return session ? (
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signout()}
+    >
       <FaGithub color="#04d361"/>
-      Henrique Campaner
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon}/>
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signIn('github')}
+    >
       <FaGithub color="#eba417"/>
       Sign in with Github
     </button>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,7 +24,7 @@ export default function Home({product}: HomeProps) {
     <main className={styles.contentContainer}>
       <section className={styles.hero}>
         <span>👏 Hey, welcome</span>
-        <h1>New about the <span>React</span> world.</h1>
+        <h1>News about the <span>React</span> world.</h1>
         <p>
           Get access to all the publications <br/>
           <span>for {product.amount} month</span>
