f7f1e9b1d7a8ddb675541e1ce8407901db91cea6
Henrique Campaner <henrique.campaner@live.com>
2021-04-28 10:02:03 +0100

    add webhooks

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,6 @@
 import { signIn, useSession } from 'next-auth/client'
+import { api } from '../../services/api'
+import { getStripeJs } from '../../services/stripeWeb'
 import styles from './subscribeButton.module.scss'
 
 interface SubscribeButtonProps {
@@ -7,10 +9,24 @@ interface SubscribeButtonProps {
 
 export function SubscribeButton({priceId}: SubscribeButtonProps) {
   const [session] = useSession()
-  function handleSubscribe() {
+  async function handleSubscribe() {
     if(!session) {
       signIn('github')
       return
+    }
+
+    try {
+      const response = await api.post('/subscribe')
+
+      const {sessionId} = response.data
+
+      const stripe = await getStripeJs()
+
+      await stripe.redirectToCheckout({
+        sessionId
+      })
+    } catch (error) {
+      alert(error.message)
     }
   }
 
